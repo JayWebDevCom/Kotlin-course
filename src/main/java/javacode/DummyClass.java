acab0318f9153ef3b529c9ec72d271ea34ef5cd8
JayWebDevCom <jaiye.s@gmail.com>
2019-01-12 18:30:18 +0000

    Learn Array

--- src/main/java/javacode/DummyClass.java
+++ src/main/java/javacode/DummyClass.java
@@ -4,4 +4,10 @@ public class DummyClass {
     public String isVacationTime(boolean onVacation) {
         return onVacation ? "I'm on vacation" : "I'm not on vacation";
     }
+
+    public void printsAnArray(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("number is: " + numbers[i]);
+        }
+    }
 }
--- src/main/kotlin/datatypes/DataTypes.kt
+++ src/main/kotlin/datatypes/DataTypes.kt
@@ -29,4 +29,57 @@ fun main(args: Array<String>) {
     val vacation = false
     val vacationTime = DummyClass().isVacationTime(vacation)
     println(vacationTime)
+
+    /*
+    Array
+     */
+    val names = arrayOf("John", "Joe", "James", "Jessica")
+    val ints = arrayOf(123, 234, 345, 456)
+    println("ints is array of long ${ints is Array<Int>}")
+
+    val longs = arrayOf<Long>(123, 234, 345, 456)
+    println("ints is array of long ${longs is Array<Long>}")
+
+    val longs2 = arrayOf(123L, 234L, 345L, 456L)
+    println("ints is array of long ${longs is Array<Long>}")
+
+    // Array indexing
+    println(longs[2])
+
+    // initialize an array of even numbers of size 16
+    val evenNumbers = Array(16) { i -> i * 2 }
+    evenNumbers.forEach { print("$it,") }
+
+    val allZeros = Array(100) { 0 }
+
+    var someArray: Array<Int>
+    someArray = arrayOf(1, 3, 5, 7)
+    someArray.forEachIndexed { i, v ->
+        print("${i + v},")
+    }
+
+    // init an array with incrementing 10s
+    val tens = Array(10) { i ->
+        (i + 1) * 10
+    }
+
+    println()
+    tens.forEach {
+        print("$it,")
+    }
+
+    println()
+    val mixedArray = arrayOf("string", 10.5, 'c')
+    mixedArray.forEach { print("$it,") }
+
+    val intArray = arrayOf(1, 2, 3, 4, 5, 6)
+    // need to use intArray or intArrayOf()
+    DummyClass().printsAnArray(intArray.toIntArray())
+
+    // at declaration time cant say size
+    // var anotherArray = Array<Int>(5);
+
+    var someOtherArray = intArrayOf(5)
+
+    intArray.toIntArray().toTypedArray()
 }
