293d08228f3d3aa0c97c5770967f6dc732c996c5
JayWebDevCom <jaiye.s@gmail.com>
2019-02-24 18:35:46 +0000

    Bicycles challenge

--- AnotherModule/src/anothercom/AnotherFile.kt
+++
@@ -1,3 +0,0 @@
-package anothercom
-
-fun topLevel(str: String) = "This is AnotherModule AnotherFile: $str"
--- src/main/kotlin/com/Main.kt
+++ src/main/kotlin/com/Main.kt
@@ -1,12 +1,44 @@
 package com
 
-import anothercom.topLevel
+import com.bicyclechallenge.Bicycle
+import com.bicyclechallenge.MountainBike
+import com.bicyclechallenge.RoadBike
 
 fun main(args: Array<String>) {
-    // 'internal' access modifier - visible within same module
-    // 'private' access modifier - visible within same file
 
-    // import from another module AnotherModule
-    // file - module settings, dependencies, +
-    println(topLevel("in the kotlinlearning module"))
+    val bicycle = Bicycle(3, 4, 5)
+    bicycle.cadence
+    bicycle.printDescription()
+    println()
+
+    val bicycle2 = Bicycle(3, 4)
+    bicycle2.cadence
+    bicycle2.printDescription()
+    println()
+
+    val mountainBike = MountainBike(2, 3, 4, 5)
+    mountainBike.seatHeight
+    mountainBike.printDescription()
+    println()
+
+    val mountainBike2 = MountainBike(2, 3, 4)
+    mountainBike2.seatHeight
+    mountainBike2.printDescription()
+    println()
+
+    val mountainBike3 = MountainBike("orange", 2, 3, 4)
+    mountainBike3.seatHeight
+    mountainBike3.printDescription()
+    println()
+
+    MountainBike.availableColours.forEach {
+        println(it)
+    }
+
+    val roadBike2 = RoadBike(2, 3, 4)
+    roadBike2.tyreWidth
+    roadBike2.printDescription()
+    roadBike2.speedUp(4)
+    roadBike2.printDescription()
+    println()
 }
